b12672a06efb6efac740194fc333a702e04b3ef0
Irina <vega_is@mail.ru>
2024-10-01 11:27:03 +0400

    1.4

--- 1/1_4.py
+++ 1/1_4.py
@@ -1,6 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+def emp_sum (data, st_index, lenght):
+    
+    tot_sum=0.0 
+    for i in range(st_index, st_index+lenght):
+        tot_sum += data [i]   
+    
+    emp = tot_sum/lenght
+   
+    return emp
 
+def calculate_variance(data, start_index, length):
+
+    # извлекаем участок данных для анализа
+    segment = data[start_index:start_index + length]
+    
+    # среднее значение
+    mean = sum(segment) / length
+    
+    # дисперсия
+    variance = sum((x - mean) ** 2 for x in segment) / length
+    
+    return variance
 # параметры
 N = 10000  # общ количество отсчетов
 mean = 1   # среднее значение
@@ -10,16 +31,17 @@ std_dev = 1  # стандартное отклонение (дисперсия =
 data = np.random.normal(loc=mean, scale=std_dev, size=N)
 
 # длины обрабатываемых участков
-lengths = np.arange(1, N + 1)
+lengths = np.arange(100, 10000 + 1,100)
 
 # инициализация массивов для хранения оценок среднего и дисперсии
-means = np.zeros(N)
-variances = np.zeros(N)
+means = np.zeros(100)
+variances = np.zeros(100)
 
 # вычисление среднего и дисперсии для каждого участка длиной от 1 до N
-for L in lengths:
-    means[L - 1] = np.mean(data[:L])
-    variances[L - 1] = np.var(data[:L])
+for i,L in enumerate (lengths):
+    means[i] = np.mean(data[:L])
+    # means[i] = calculate_variance(data[:L],0,L)
+    variances[i] = emp_sum(data[:L],0,L)
 
 # построение графиков
 plt.figure(figsize=(14, 6))
